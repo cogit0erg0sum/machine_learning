fa86b6a7986e4e5e94edce47bfdc2ec8a7d941b1
Harshit <harshitsrivastava19aug@gmail.com>
2022-06-26 03:34:31 +0530

    added logger file

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from housing.logger import logging
 
 app = Flask(__name__)
 
 @app.route("/", methods =['GET' , 'POST'])
 def index():
+    logging.info("We are testing logging module")
     return 'Starting my first ever ML Project. So Excited!!! CI-CD pipeline established'
 
 if __name__ == "__main__":
