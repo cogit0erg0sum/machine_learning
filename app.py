abe5b6dbd287d937399a6763ab386cdea3e247f3
Harshit <harshitsrivastava19aug@gmail.com>
2022-06-26 04:00:33 +0530

    exception file created

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from housing.logger import logging
+from housing.exception import HousingException
 
 app = Flask(__name__)
 
 @app.route("/", methods =['GET' , 'POST'])
 def index():
-    logging.info("We are testing logging module")
+    try:
+        raise Exception("Trying to raise excepton")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
     return 'Starting my first ever ML Project. So Excited!!! CI-CD pipeline established'
 
 if __name__ == "__main__":
