225751b7061d30be0209dc210b73412a978d7393
Harshit <harshitsrivastava19aug@gmail.com>
2022-06-26 13:11:47 +0530

    modified app.py

--- app.py
+++ app.py
@@ -6,12 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods =['GET' , 'POST'])
 def index():
-    try:
-        raise Exception("Trying to raise excepton")
-    except Exception as e:
-        housing = HousingException(e, sys)
-        logging.info(housing.error_message)
-        logging.info("We are testing logging module")
+    
     return 'Starting my first ever ML Project. So Excited!!! CI-CD pipeline established'
 
 if __name__ == "__main__":
