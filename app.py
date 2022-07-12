2e5745e47073bf600e8fdae340b9911edc65e8a6
Harshit <harshitsrivastava19aug@gmail.com>
2022-07-12 08:53:09 +0530

    exception and log info

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask
 import sys
 from housing.logger import logging
+from housing.exception import HousingException
 
 
 app = Flask(__name__)
 
 @app.route("/", methods =['GET' , 'POST'])
 def index():
-    logging.info("checking logging file configpython a")
+    try:
+        raise Exception("we are testing exception") 
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
+        
+    
     return 'Starting my first ever ML Project. So Excited!!! CI-CD pipeline established'
 
 if __name__ == "__main__":
--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -1,5 +1,33 @@
 import os
 import sys
 
-class HousingException:
-    pass
+class HousingException(Exception):
+    """
+    error_message = Exception object
+    error_detail: object of sys module
+    """
+    
+    def __init__(self, error_message: Exception, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message
+        
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception,error_detail:sys)-> str:
+        _,_,exec_tb = error_detail.exc_info()
+        
+        
+        file_name = exec_tb.tb_frame.f_lineno
+        file_name = exec_tb.tb_frame.f_code.co_filename
+        
+        error_message = f"error occured in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
+        
+        return error_message
+    
+    
+    def __str__(self):
+        return self.error_message
+    
+    def __repr__(self) -> str:
+        return HousingException.__name__.str()
+        
+    
