7062ec8437b896cfa2a3211f4dd8b060d8a2aeee
Harshit <harshitsrivastava19aug@gmail.com>
2022-07-18 21:14:47 +0530

    added config info

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -2,20 +2,29 @@ from housing.entity.config_entity import DataInjestionConfig, DataTransformation
 
 from housing.util.util import read_yaml_file
 from housing.constant import *
+from housing.exception import HousingException
+import sys, os
+from housing.logger import logging
+ 
 
 
 
 
 
-class configuration:
+class Configuration:
     
     def __init__(self,
-                 config_file_path = CONFIG_FILE_PATH,
+                 config_file_path: str= CONFIG_FILE_PATH,
                  current_time_stamp:str = CURRENT_TIME_STAMP) -> None:
         
-        self.config_info = read_yaml_file(file_path = config_file_path)
-        self.training_pipeline_config = self.get_training_pipeline_config()
-        self.time_stamp = current_time_stamp
+        try:
+            
+        
+            self.config_info = read_yaml_file(file_path = config_file_path)
+            self.training_pipeline_config = self.get_training_pipeline_config()
+            self.time_stamp = current_time_stamp
+        except Exception as e:
+            raise HousingException(e,sys) from e
         
         pass
     
@@ -45,4 +54,13 @@ class configuration:
     
     
     def get_training_pipeline_config(self) -> TrainingPipelineConfig:
-        pass
+        try:
+            training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
+            artifact_dir = os.path.join(ROOT_DIR, 
+                                        training_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
+                                        training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY])
+            training_pipeline_config = TrainingPipelineConfig(artifact_dir= artifact_dir)
+            logging.info(f"Training pipeline config : {training_pipeline_config}")
+            return training_pipeline_config
+        except Exception as e:
+            raise HousingException(e,sys) from e
